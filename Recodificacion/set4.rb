0177a3e1a885af12c9ebe5497e72ae2b40226c9b
manu <manulito85@hotmail.com>
2015-02-03 15:10:59 -0300

    borrados los set

--- Recodificacion/set4.rb
+++
@@ -1,7 +0,0 @@
-/SETEO DE SCRIPT/
-
-@ctr="C:\\atsc\\control_reco.txt"
-@log="c:\\atsc\\log_reco.txt"
-@control_macros_gpym="c:\\atsc\\control_macros_gpym.txt"
-
-@lote="\\\\cgtec\\prueba\\recodificar4.txt"
