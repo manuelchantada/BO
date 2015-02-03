ea9afc650bde1aa19d7a86d98497d16056b75532
unknown <mumolij@TPHP3003.tasa.telefonica.com.ar>
2015-02-03 11:47:44 -0300

    parcheado el error de linea 45

---
+++ Navegacion/set.rb
@@ -0,0 +1,25 @@
+/SETEO DE SCRIPT/
+#USER Y PASS
+@user="xfl384"
+@pass="Culpina20142"
+@usert="xfl072"
+@passt="Portal032014"
+#LOTE/
+@lote="\\\\Tphp3008\\PRUEBA\\REG.txt"
+#SETEO DE CAPTURAS/
+@captura="c:\\atsc\\captura.txt"
+#CONTROL/
+@ctr="c:\\atsc\\control.txt"
+@ctrl="c:\\atsc\\control_macros_gpym.txt"
+@pc="TPHC3779"
+@pdr="PDR"
+#TAMANIO Y POSICION/
+@width=640	
+@height=1024
+@xpos=0
+@ypos=0	
+#LOG DE ERRORES/
+@log="c:\\atsc\\log.txt"
+#HORARIOS
+@horai=8
+@horaf=20
---
+++ Navegacion/set4.rb
@@ -0,0 +1,25 @@
+/SETEO DE SCRIPT/
+#USER Y PASS
+@user="xfl497"
+@pass="Portal1621"
+@usert="xfl631"
+@passt="Portal2015"
+#LOTE/
+@lote="\\\\Tphp3008\\PRUEBA\\REG4.txt"
+#SETEO DE CAPTURAS/
+@captura="c:\\atsc\\captura.txt"
+#CONTROL/
+@ctr="c:\\atsc\\control.txt"
+@ctrl="c:\\atsc\\control_macros_gpym.txt"
+@pc="PH3918P"
+@pdr="PDR4"
+#TAMANIO Y POSICION/
+@width=640	
+@height=1024
+@xpos=0
+@ypos=0	
+#LOG DE ERRORES/
+@log="c:\\atsc\\log.txt"
+#HORARIOS
+@horai=8
+@horaf=20
